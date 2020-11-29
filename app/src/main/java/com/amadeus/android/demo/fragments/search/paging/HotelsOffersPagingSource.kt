79c30446e3dafd72343c573fade11c0939d90d16
Nicolas Pietri <nico.pietri@gmail.com>
2020-11-29 21:43:02 +0100

    Update paging source with comments

--- app/src/main/java/com/amadeus/android/demo/fragments/search/paging/HotelsOffersPagingSource.kt
+++ app/src/main/java/com/amadeus/android/demo/fragments/search/paging/HotelsOffersPagingSource.kt
@@ -3,6 +3,7 @@ package com.amadeus.android.demo.fragments.search.paging
 import androidx.paging.PagingSource
 import com.amadeus.android.ApiResult
 import com.amadeus.android.demo.SampleApplication
+import com.amadeus.android.demo.fragments.search.HotelsOffersViewModel
 import com.amadeus.android.domain.resources.HotelOffer
 
 class HotelsOffersPagingSource(
@@ -14,14 +15,21 @@ class HotelsOffersPagingSource(
     private val amadeus = SampleApplication.amadeus
 
     override suspend fun load(params: LoadParams<ApiResult.Success<List<HotelOffer>>>): LoadResult<ApiResult.Success<List<HotelOffer>>, HotelOffer> {
-        val key = params.key
+        val key: ApiResult.Success<List<HotelOffer>>? = params.key
         val response = when {
+            // We should load next page
             params is LoadParams.Append && key != null -> amadeus.next(key)
+            // We should load previous page
             params is LoadParams.Prepend && key != null -> amadeus.previous(key)
+            // We should refresh using `amadeus.self()` only if key is not null
+            params is LoadParams.Refresh && key != null -> amadeus.self(key)
+            // Key is null, it's a cold start for the lib, we should query the api
             else -> amadeus.shopping.hotelOffers.get(
                 cityCode = destination,
                 checkInDate = checkInDate,
-                checkOutDate = checkOutDate
+                checkOutDate = checkOutDate,
+                // We use the same page size set in PageConfig
+                pageLimit = HotelsOffersViewModel.NETWORK_PAGE_SIZE
             )
         }
         return when (response) {
